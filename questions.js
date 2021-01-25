df1bafaed8e53268ddc6c07f7ea5a182e87418c5
Fabian <fabianffc@gmail.com>
2021-01-25 07:41:44 -0600

    setting up my html pages

---
+++ questions.js
@@ -0,0 +1,22 @@
+
+
+var questionarray=[{
+    title:"What is the basic piece in chess?", 
+    choices:["pawn", "queen", "knight", "king"],
+    anwser: 0,
+},
+{
+    title:"How many pieces are in total in chess?", 
+    choices:["24", "16", "32", "34"],
+    anwser: 2,
+},
+{
+    title:"How many squares can the king move at one time?", 
+    choices:["3", "2", "1", "4"],
+    anwser: 2,
+},
+{
+    title:"Wich piece in chess is the only animal ?", 
+    choices:["bear", "lion", "horse", "giraffe"],
+    anwser: 2,
+}]
