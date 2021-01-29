46b7ed754bcb2b68886bfb295b6881768de28b1e
Fabian <fabianffc@gmail.com>
2021-01-29 08:27:08 -0600

    array of questions

--- script.js
+++ script.js
@@ -10,6 +10,71 @@
 // WHEN the game is over
 // THEN I can save my initials and score
 
+//We start for grabbing the question html elements with the classes question n choices
+const question = document.querySelector(".question");
+const choices = Array.from(document.querySelectorAll(".choices"));
+
+
+let currentQuestion = [];
+let acceptAnswer = true;
+
+//Set up the parameters to start the game
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+
+//Questions array where we are gonna pull our info
+let questions = [
+    {
+        question: "What is the basic piece in chess?",
+        choice1: "pawn ♟",
+        choice2: "queen 👸",
+        choice3: "knight ♞",
+        choice4: "king 🤴",
+        answer: 1
+    },
+    {
+        question: "How many pieces are in total in chess?",
+        choice1: "24",
+        choice2: "16",
+        choice3: "32",
+        choice4: "34",
+        answer: 3,
+    },
+    {
+        question: "How many squares can the king move at one time?",
+        choice1: "3",
+        choice2: "2",
+        choice3: "1",
+        choice4: "4",
+        answer: 3,
+    },
+    {
+        question: "Wich piece in chess is the only animal ?",
+        choice1: "bear 🐻",
+        choice2: "lion 🦁",
+        choice3: "giraffe 🦒",
+        choice4: "horse 🐴",
+        answer: 4
+    }
+];
+
+const correctBonus = 10;
+const maxQuestions = 4;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions]
+    console.log(availableQuestions);
+}
+getNewQuestion = () =>{
+    questionCounter++;
+    
+}
+
+startGame();
 
 
 
@@ -38,29 +103,3 @@
 
 
 
-
-
-
-
-
-//Questions
-const questions = [{
-    title:"What is the basic piece in chess?", 
-    choices:["pawn", "queen", "knight", "king"],
-    answer: "pawn",
-},
-{
-    title:"How many pieces are in total in chess?", 
-    choices:["24", "16", "32", "34"],
-    answer: "32",
-},
-{
-    title:"How many squares can the king move at one time?", 
-    choices:["3", "2", "1", "4"],
-    answer: "1",
-},
-{
-    title:"Wich piece in chess is the only animal ?", 
-    choices:["bear", "lion", "horse", "giraffe"],
-    answer: "horse",
-}];
