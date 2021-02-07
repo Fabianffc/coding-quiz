ede18de8f18cf59dd69992304fb003f3c3370140
Fabian <fabianffc@gmail.com>
2021-02-06 19:00:50 -0600

    adding initials to localstorage

---
+++ final.js
@@ -0,0 +1,30 @@
+const initialsButton = document.querySelector(".initials-button");
+let initialsList = localStorage.getItem("initials") ? JSON.parse(localStorage.getItem("initials")) : []
+
+initialsButton.addEventListener("click", function(){
+    const initialsInput = document.querySelector(".initialsInput");
+    initialsList.push(initialsInput.value)
+    localStorage.setItem("initials", JSON.stringify(initialsList)) 
+    initialsInput.value = ""
+
+    const initialDiv = document.querySelector(".initialDiv");
+    initialDiv.classList.add("hide")
+
+    let scoreDiv = document.querySelector(".scoreDiv");
+    scoreDiv.classList.remove("hide")
+
+    let initialsLocalStorage = document.querySelector(".initialsLocalStorage");
+    
+
+
+    for(var i = 0; i<initialsList.length; i++){
+            initialsLocalStorage.innerHTML =  initialsLocalStorage.innerHTML + `
+            <li class="list-group-item">${initialsList[i]}</li>
+                       
+            ` 
+            
+    }
+
+
+})
+
--- script.js
+++ script.js
@@ -94,7 +94,6 @@ choices.forEach(choice => {
     });
 });
 
-
 startGame();
 
 
